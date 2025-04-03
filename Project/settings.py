1313589209170a52135214fdbefbed3bcac7c26d
Mhhaider872 <haidermo878@gmail.com>
2025-04-03 16:20:42 +0530

    'changes'

--- Project/settings.py
+++ Project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+9bdia3-n2#icmo@ogfk#nj4+7c!3b7q39w+0141nf#qkg)(hy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
